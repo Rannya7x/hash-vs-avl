c1022bfbb3c755c763a6f278304a15c39c923d85
Rayana <rannya7x@gmail.com>
2025-12-10 18:10:16 -0400

    feat: add file reading and data insertion

--- src/hashXavl.c
+++ src/hashXavl.c
@@ -9,7 +9,7 @@ int main_teste_leitura(){
     /*Primeiro teste de leitura das arquivos.in*/
 
     FILE* medidas = fopen("../medidas_2_05.in", "r");
-    
+
     if (medidas == NULL){
         perror("Erro ao abrir o arquivo de medidas");
         return 0;
@@ -107,18 +107,49 @@ int main(){
         printf("----- Iniciando experimento -----\n");
 
         const char* instancia_atual = instancias[i];
-
         printf("Processando instância: %s\n", instancia_atual);
 
         //1. Inicialização da Hash, Avl e Metricas
         Metrics* hash_metrics = init_metrics();
         Metrics* avl_metrics = init_metrics();
 
-        //Inicialização da Hash e AVL
         t_hash* hash = hash_criar(hash_metrics);
         t_avl* avl = avl_criar(imprimir_mme, comparar_mme, avl_metrics);
 
         //2. Leitura do arquivo e inserção na Hash/AVL
+         
+        FILE* medidas = fopen(instancia_atual, "r");
+        if(medidas == NULL){
+            perror("Error ao abrir o arquivo de medidas");
+            exit(EXIT_FAILURE);
+        }
+        //Loop de inserção dos dados na Hash/AVL
+        t_registro_sensor* sensor; //armazena o registro lido do arquivo
+        t_info_consolidada* info = NULL;
+        while ((sensor = ler_sensor(medidas))!=NULL){//Enquanto houver registros a serem lidos
+            //Geração da chave hash
+            char* hora = extrair_hora(sensor->horario);
+            int chave = gerar_chave_int(sensor->id, hora);
+            /*Busca do MME
+            Se a busca retornar NUll, significa que é a primeira medição para aquele sensor na hora
+                - add a medição atual como MME na hash/AVL
+            Se a busca retornar um valor válido, usamos a MME retornada para calcular a nova MME
+                - atualizar a hash/AVL com a nova MME*/
+            info = (t_info_consolidada*)hash_buscar(hash, chave);//a hash geralmente é mais rapida
+            if(info == NULL){
+                t_info_consolidada* info = criar_info_consolidada(chave, sensor->valor, sensor->id, hora);
+                double mme_inicial = sensor->valor;
+                hash_inserir(hash, chave, info);
+                avl_inserir(avl, info);
+            }else{
+                double mme_suavizada = calcular_mme(info_hash->mme_suavizada, sensor->valor, sensor->alpha);
+                info_hash->mme_suavizada = mme_suavizada;
+                //hash_atualizar(hash, chave, info);
+            }
+
+        }
+
+
         //3. Consolidação das medições (Cálculo da MME)
         //4. Coleta e exibição das métricas
         //5. Liberação de memória e finalização do experimento
