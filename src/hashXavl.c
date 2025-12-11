7022be81d49661718fcd21f5ddbb04abcc5f821f
Rayana Almeida <rayana.almeida@icomp.ufam.edu.br>
2025-12-11 10:35:35 -0400

    feat: add data insertion loop in hash and calculate MME

--- src/hashXavl.c
+++ src/hashXavl.c
@@ -5,96 +5,6 @@
 #include "./include/utils.h"
 #include "./include/metrics.h"
 
-int main_teste_leitura(){
-    /*Primeiro teste de leitura das arquivos.in*/
-
-    FILE* medidas = fopen("../medidas_2_05.in", "r");
-
-    if (medidas == NULL){
-        perror("Erro ao abrir o arquivo de medidas");
-        return 0;
-    }
-    t_registro_sensor* sensor;
-
-    printf("Iniciando leitura dos sensores do arquivo...\n");
-
-    int contador = 0;
-    while((sensor = ler_sensor(medidas))!=NULL){
-        contador++;
-
-        //Validação simples da leitura
-        printf(
-            "Sensor %d: ID=%d, Tipo=%d, Valor=%.3f, Horario=%s, Alpha=%.2f\n",
-            contador,
-            sensor->id,
-            sensor->tipo,
-            sensor->valor,
-            sensor->horario,
-            sensor->alpha
-        );
-        free(sensor); //liberar memória se não for inserido na hash ou AVL    
-    }
-    fclose(medidas);
-    printf("Leitura finalizada. Total de sensores lidos: %d\n", contador); 
-
-    return 0;
-}
-int main_teste_extrair_hora(){
-    /*Teste da função de extrair hora*/
-    char* horario_teste[] = {
-        "14:30:45",
-        "09:15:00",
-        "23:59:59",
-        "00:00:00"
-    };
-    int num_testes = sizeof(horario_teste) / sizeof(horario_teste[0]);
-
-    for(int i=0; i<num_testes; i++){
-        char* hora_extraida = extrair_hora(horario_teste[i]);
-        if(hora_extraida != NULL){
-            printf("Horario original: %s -> Hora extraida: %s\n", horario_teste[i], hora_extraida);
-            free(hora_extraida);
-        }else{
-            printf("Falha ao extrair hora do horario: %s\n", horario_teste[i]);
-        }
-    }
-    printf("--------------------------------\n");
-    return 0;
-}
-int main_teste_mme(){
-    /*Teste de Calculo da Media Movel Exponencial*/
-
-    //Ex:
-    const double alpha = 0.40; //Fator de suavização
-
-    double medicoes[] = {100.0, 120.0, 90.0, 150.0};
-
-    //caso base: t=1
-    double media_suavizada = 0.0;
-
-    printf("Cálculo da Média Móvel Exponencial (MME) com alpha=%.2f\n", alpha);
-
-    
-    int t = 1;
-    for(int i=0;i<sizeof(medicoes)/sizeof(medicoes[0]);i++){
-        double medicao_atual = medicoes[i];//medição no tempo t
-        double media_anterior = media_suavizada;
-        media_suavizada = calcular_mme(media_anterior, medicao_atual, alpha);
-
-        if(t==1){
-            //Caso base: m_t = medicao_atual
-            printf("t=%d: Medição=%.2f -> MME=%.2f (caso base)\n", t, medicao_atual, media_suavizada);
-        } else{
-            double esperado = alpha * medicao_atual + (1.0 - alpha) * media_anterior; 
-            printf("t=%d Med. Atual=%.2f, MME_ant=%.4f. MME_nova = %.4f (Esperado: %.4f)\n", t, medicao_atual, media_anterior, media_suavizada, esperado);
-        }
-        t++;
-    }
-    printf("-----------------------------------------\n");
-}
-
-
-//Vetor de nomes das 4 instâncias de teste
 const char* instancias[] = {
     "medidas_2_05.in",
     "medidas_2_10.in",
@@ -116,40 +26,51 @@ int main(){
         t_hash* hash = hash_criar(hash_metrics);
         t_avl* avl = avl_criar(imprimir_mme, comparar_mme, avl_metrics);
 
-        //2. Leitura do arquivo e inserção na Hash/AVL
+        //2. Leitura do arquivo e inserção na Hash
          
         FILE* medidas = fopen(instancia_atual, "r");
         if(medidas == NULL){
             perror("Error ao abrir o arquivo de medidas");
             exit(EXIT_FAILURE);
         }
-        //Loop de inserção dos dados na Hash/AVL
-        t_registro_sensor* sensor; //armazena o registro lido do arquivo
+        //Loop de inserção dos dados na Hash
+        t_registro_sensor* sensor_hash;
         t_info_consolidada* info = NULL;
-        while ((sensor = ler_sensor(medidas))!=NULL){//Enquanto houver registros a serem lidos
+        printf("Iniciando inserção na Hash...\n");
+        while ((sensor_hash = ler_sensor(medidas))!=NULL){//Enquanto houver registros a serem lidos
             //Geração da chave hash
-            char* hora = extrair_hora(sensor->horario);
-            int chave = gerar_chave_int(sensor->id, hora);
+            int* hora_ptr = extrair_hora(sensor_hash->horario);
+            int chave = gerar_chave_int(sensor_hash->id, hora_ptr);
+            int hora = *hora_ptr; 
+            free(hora_ptr);
             /*Busca do MME
-            Se a busca retornar NUll, significa que é a primeira medição para aquele sensor na hora
-                - add a medição atual como MME na hash/AVL
+            Se a busca retornar NUll, significa que é a primeira medição para aquele sensor_hash na hora
+                - add a medição atual como MME na hash
             Se a busca retornar um valor válido, usamos a MME retornada para calcular a nova MME
-                - atualizar a hash/AVL com a nova MME*/
+                - atualizar a hash com a nova MME*/
             info = (t_info_consolidada*)hash_buscar(hash, chave);//a hash geralmente é mais rapida
             if(info == NULL){
-                t_info_consolidada* info = criar_info_consolidada(chave, sensor->valor, sensor->id, hora);
-                double mme_inicial = sensor->valor;
+                info = criar_info_consolidada(chave, sensor_hash->valor, sensor_hash->id, hora, sensor_hash->alpha);
                 hash_inserir(hash, chave, info);
-                avl_inserir(avl, info);
             }else{
-                double mme_suavizada = calcular_mme(info_hash->mme_suavizada, sensor->valor, sensor->alpha);
-                info_hash->mme_suavizada = mme_suavizada;
-                //hash_atualizar(hash, chave, info);
+                double nova_mme = calcular_mme(info->mme_suavizada, sensor_hash->valor, info->alpha);
+                info->mme_suavizada = nova_mme;
             }
-
+            free(sensor_hash);
+            printf("Inserção na Hash concluída para chave %d.\n", chave);
         }
 
+        //Loop da avl
+        rewind(medidas);
+        printf("Ponteiro do arquivo reiniciado para inserção na AVL.\n");
+        printf("Iniciando inserção na AVL...\n");
 
+       /* while(true){
+
+        }*/
+        
+        fclose(medidas);
+        printf("Medidas fechadas.\n");
         //3. Consolidação das medições (Cálculo da MME)
         //4. Coleta e exibição das métricas
         //5. Liberação de memória e finalização do experimento
